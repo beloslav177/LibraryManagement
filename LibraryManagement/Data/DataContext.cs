e6ba396421478b7b7f667eb7cc3edd42ab2b98a6
Lukas Belay <lukas.belay@vixtechnology.com>
2022-02-12 11:50:28 +0100

    Add project files.

---
+++ LibraryManagement/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Library.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class DataContext : DbContext
+    {
+        private const string connectionString = @"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;";
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.();
+        }
+    }
+}
---
+++ LibraryManagement/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string AuthorFirstName { get; set; }
+
+        public string AuthorLastName { get; set; }
+
+        public string BookName { get; set; }
+    }
+}
